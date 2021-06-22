25886b4f64b1e442a12da5052803bc311efb7cdd
thomas blas <thomas-blas@live.fr>
2021-06-22 23:17:42 +0200

    dog controller ok

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,13 +1,45 @@
 class DogsController < ApplicationController
-  def create
+  def new
+    @dog = Dog.new
     authorize @dog
   end
 
-  def destroy
+  def create
+    dog = Dog.new(dog_params)
+    dog.user = current_user
+    authorize dog
+    if dog.save
+      redirect_to dashboard_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    id = params[:id]
+    @dog = Dog.find(id)
     authorize @dog
   end
 
   def update
+    id = params[:id]
+    @dog = Dog.find(id)
     authorize @dog
+    @dog.update(dog_params)
+    redirect_to dashboard_path
+  end
+
+  def destroy
+    id = params[:id]
+    dog = Dog.find(id)
+    authorize @dog
+    dog.destroy
+    redirect_to dashboard_path
+  end
+
+  private
+
+  def dog_params
+    params.require(:dog).permit(:name, :age, :size, :gender, :photo_key, :user_id)
   end
 end
