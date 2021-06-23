686aa0ba28775dc72a50fa31ae45c47caa989737
Christian Huang <christian.huang.49@gmail.com>
2021-06-23 10:32:50 +0200

    Event has user and user have events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :walk
   has_many :messages
+  has_many :event_has_users
   has_many :users, through: :event_has_users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   has_many :dogs, dependent: :destroy
   has_many :reviews, dependent: :destroy
   has_many :messages
+  has_many :event_has_users
   has_many :events, through: :event_has_users
   has_one_attached :photo
 end
