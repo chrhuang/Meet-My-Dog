e885ef507dbdadf52e63ec12caddd460048cc0ed
Thomas Petitprez <t.petitprez@outlook.fr>
2021-06-23 13:11:07 +0200

    search bar WIP

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,6 +1,12 @@
 class SitesController < ApplicationController
   def index
     @sites = policy_scope(Site).order(created_at: :desc)
+    @markers = @sites.geocoded.map do |site|
+      {
+        lat: site.lat,
+        lng: site.long
+      }
+    end
   end
 
   def show
--- app/models/site.rb
+++ app/models/site.rb
@@ -2,4 +2,7 @@ class Site < ApplicationRecord
   has_many :site_photos, dependent: :destroy
   has_many :walks, dependent: :destroy
   has_many :reviews, dependent: :destroy
+
+  reverse_geocoded_by :lat, :long
+  after_validation :reverse_geocode, if: ->(obj) { obj.lat.present? and obj.long.present? }  # auto-fetch address
 end
