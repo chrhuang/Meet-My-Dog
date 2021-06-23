52d3f739a11777021f950000c4bc2401fe62e316
Christian Huang <christian.huang.49@gmail.com>
2021-06-23 20:12:38 +0200

    Algolia completed

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -2,8 +2,12 @@ import places from 'places.js'
 
 const initAutocomplete = () => {
   const addressInput = document.getElementById('banner-search')
+  const addressInputNavbar = document.getElementById('navbar-search')
   if (addressInput) {
     places({ container: addressInput })
+  }
+  if (addressInputNavbar) {
+    places({ container: addressInputNavbar })
   }
 }
 
