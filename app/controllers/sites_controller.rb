2eebc6a2de5b7a1ec7258018cca037811ee061a9
thomas blas <thomas-blas@live.fr>
2021-06-24 13:44:28 +0200

    custom marker for sites not city

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,11 +1,13 @@
 class SitesController < ApplicationController
   def index
+    @sites = Site.geocoded
     @sites = policy_scope(Site).order(created_at: :desc)
     @markers = @sites.geocoded.map do |site|
       {
         lat: site.lat,
         lng: site.long,
-        info_window: render_to_string(partial: "site_window", locals: { site: site })
+        info_window: render_to_string(partial: "site_window", locals: { site: site }),
+        image_url: helpers.asset_url('Component_2-removebg-preview.png')
       }
     end
   end
