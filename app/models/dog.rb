307181d373d603b3db59d5f7640b1d2663acf88c
Christian Huang <christian.huang.49@gmail.com>
2021-06-25 15:01:24 +0200

    chat integration completed

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,5 +1,6 @@
 class Dog < ApplicationRecord
   belongs_to :user
   has_many :temperaments, through: :dog_has_temperaments
+  has_many :dog_has_temperaments, dependent: :destroy
   has_one_attached :photo
 end
