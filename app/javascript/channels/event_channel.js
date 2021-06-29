0ffff5cab3ae49c27e6127f069eba0b396449f41
Christian Huang <christian.huang.49@gmail.com>
2021-06-29 16:33:12 +0200

    notif

--- app/javascript/channels/event_channel.js
+++ app/javascript/channels/event_channel.js
@@ -2,11 +2,12 @@ import consumer from './consumer'
 
 const initEventCable = () => {
   const messagesContainer = document.getElementsByClassName('messages-body')
-  console.log(messagesContainer)
   for (let i = 0; i < messagesContainer.length; i++) {
     const id = messagesContainer[i].dataset.eventId
     consumer.subscriptions.create({ channel: 'EventChannel', id: id }, {
-      received (data) {
+      received(data) {
+        // document.querySelector('#chat-notif').classList.remove('d-none')
+        // document.querySelector('#chat-notif').classList.add('d-block') //A voir les notifications avec le chat
         messagesContainer[i].insertAdjacentHTML('beforeend', data)
       }
     })
