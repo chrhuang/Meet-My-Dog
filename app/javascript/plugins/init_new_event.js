0c01dc45c98a4d109591a1e98f558b12fd250e1b
Christian Huang <christian.huang.49@gmail.com>
2021-06-30 15:39:56 +0200

    Balade seul

--- app/javascript/plugins/init_new_event.js
+++ app/javascript/plugins/init_new_event.js
@@ -3,7 +3,18 @@ const initNewEvent = () => {
   const inputId = document.querySelector('#event_walk_id')
   elements.forEach((elem) => {
     elem.addEventListener('click', () => {
-      inputId.value = elem.dataset.walkId
+      if (inputId) {
+        inputId.value = elem.dataset.walkId
+      }
+    })
+  })
+  const aloneElements = document.querySelectorAll('.create-alone-event')
+  const alone = document.querySelector('.walk-alone')
+  aloneElements.forEach((elem) => {
+    elem.addEventListener('click', () => {
+      if (alone) {
+        alone.value = elem.dataset.walkId
+      }
     })
   })
 }
