6100f0f8f2a304b76de5028e11d4fe43f7fd73f7
Thomas Petitprez <t.petitprez@outlook.fr>
2021-06-30 15:14:49 +0200

    coordinates added to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,7 +29,7 @@ dog.photo.attach(io: file, filename: 'tomdog2.png', content_type: 'image/png')
 
 puts "Create User 2/3"
 file = URI.open('https://avatars.githubusercontent.com/u/82810122?v=4')
-user = User.create!(email: "thomas@email.com", password: "123456", nickname: "Thomas", description: "C'est là que le Blas blesse.")
+user = User.create!(email: "thomas@email.com", password: "123456", nickname: "Thomas", description: "Pectauraux à la Broly! Gripsou fait du sale!")
 user.photo.attach(io: file, filename: 'thomas.png', content_type: 'image/png')
 file = URI.open('https://media1.fdncms.com/orlando/imager/u/original/26765334/adobestock_107224974.jpeg')
 dog = Dog.create!(name: "Poki", user: user, size: "Grand", gender: "male", age: 2)
@@ -93,32 +93,32 @@ Walk.destroy_all
 
 puts "Create Walk 1/6"
 file = URI.open('https://res.cloudinary.com/dtn8c7o7m/image/upload/v1624539419/citadelle_juin_2020_01_dw1mwi.jpg')
-walk = Walk.create!(name: "Tour à l'entrée de la citadelle", description: "balade au bord de l'eau, rapide et parfaite pour un petit chien.", length: 1, duration: 30,  site: Site.first)
+walk = Walk.create!(name: "Tour à l'entrée de la citadelle", description: "balade au bord de l'eau, rapide et parfaite pour un petit chien.", length: 6.33962, duration: 7,  site: Site.first, coords: "3.0510403488002567,50.63785293163426;3.0507831279417417,50.63825596827243;3.052447498199257,50.6397817185246;3.053476381630361,50.63981050578934;3.0510403488002567,50.63784333548142")
 walk.photo.attach(io: file, filename: 'citadelle01.png', content_type: 'image/png')
 
 puts "Create Walk 2/6"
 file = URI.open('https://parcdelacitadelle.lille.fr/sites/default/files/styles/full/public/thumbnails/image/p-01.premiere_enceintecphilippe_frutier.jpg?itok=SweekwvY')
-walk = Walk.create!(name: "Visite intérieure de la citadelle", description: "balade à l'intérieur de l'enceinte de la citadelle.", length: 5, duration: 120,  site: Site.first)
+walk = Walk.create!(name: "Visite intérieure de la citadelle", description: "balade à l'intérieur de l'enceinte de la citadelle.", length: 24.929989999999997, duration: 29,  site: Site.first, coords:"3.0507872034807235,50.63826373560909;3.047367846482615,50.640412456853824;3.048518507615313,50.6418581736562;3.0443805269024153,50.64361676861617;3.0403613378798013,50.64166008337483;3.042069639471862,50.63873723847533;3.0472171427305454,50.63883498804961;3.0473114099764587,50.63967055032734;3.0507770795808824,50.6382693208364")
 walk.photo.attach(io: file, filename: 'citadelle02.png', content_type: 'image/png')
 
 puts "Create Walk 3/6"
 file = URI.open('https://res.cloudinary.com/dtn8c7o7m/image/upload/v1624539773/145151_88829_b1w2gu.jpg')
-walk = Walk.create!(name: "Longue promenade autour de la citadelle", description: "Pour les toutous les plus sportifs, un tour complet de la citadelle.", length: 15, duration: 300,  site: Site.first)
+walk = Walk.create!(name: "Longue promenade autour de la citadelle", description: "Pour les toutous les plus sportifs, un tour complet de la citadelle.", length: 41.031459999999996, duration: 48,  site: Site.first, coords:"3.05157266288316,50.63741358159632;3.0508455335009614,50.638284774640255;3.0524357220453453,50.64092110422277;3.0521418067155537,50.64277546074564;3.0499896894020253,50.64466250952697;3.0473779003244204,50.64690614557625;3.047814369673034,50.648811198624344;3.0468249227432125,50.647999979727246;3.0442238666771573,50.646807517565605;3.0404401997552952,50.645016718162424;3.0390133007229565,50.644290815907766;3.0373714177396494,50.642170834070214;3.0371166465809836,50.64104666100411;3.0370820355314834,50.63982259479471;3.037726954135593,50.63949138176565;3.038746210959772,50.63898144643804;3.040558014279924,50.63778958265914;3.0420249583457633,50.63716673754951;3.043809866823352,50.63685652251618;3.0449648829877844,50.63696545333383;3.048193059747973,50.637569521619895;3.0506973439291585,50.63825223753619;3.0516014935886915,50.637411449900526")
 walk.photo.attach(io: file, filename: 'citadelle03.png', content_type: 'image/png')
 
 puts "Create Walk 4/6"
 file = URI.open('https://res.cloudinary.com/dtn8c7o7m/image/upload/v1624540016/NBeTz3gLM77bt4O-gR0rvTgA30k_azsdef.jpg')
-walk = Walk.create!(name: "Promenade au bord du lac", description: "Petite promenade sur la rive du lac, au calme pour se détendre les papattes.", length: 2.5, duration: 60,  site: Site.last)
+walk = Walk.create!(name: "Promenade au bord du lac", description: "Petite promenade sur la rive du lac, au calme pour se détendre les papattes.", length: 9.49, duration: 11,  site: Site.last, coords:"3.1553353960466097,50.634908708650045;3.1588574261108135,50.635904982111754;3.1604727751127655,50.63669535867868;3.160071736422964,50.636946853073454;3.1575617880321545,50.63610034803048;3.1562194649370667,50.63525807694788;3.155618516685365,50.63530889750163;3.1554090661267367,50.63528675876941;3.1553389226206434,50.63491283922957")
 walk.photo.attach(io: file, filename: 'heron01.png', content_type: 'image/png')
 
 puts "Create Walk 5/6"
 file = URI.open('https://res.cloudinary.com/dtn8c7o7m/image/upload/v1624540284/3942d8043f14e30dc57c3d3389c52d03_jzxzrz.jpg')
-walk = Walk.create!(name: "Tour du petit lac", description: "Tour du petit lac pour dire bonjour aux chevaux de traits et aux oiseaux.", length: 7, duration: 240,  site: Site.last)
+walk = Walk.create!(name: "Tour du petit lac", description: "Tour du petit lac pour dire bonjour aux chevaux de traits et aux oiseaux.", length: 28.78999, duration: 33,  site: Site.last, coords:"3.1551589248744563,50.634045745638105;3.1522814927969307,50.63432831846117;3.1522005755421105,50.63582649564185;3.148969453557868,50.635951116901225;3.1491295559148114,50.63538339719727;3.147773494694519,50.6354725227242;3.14853412553839,50.63407416309704;3.144711663999516,50.63522830925663;3.1437493307813043,50.63416451835374;3.1445100322775374,50.6336703996372;3.148898651030862,50.633690773884524;3.152766240948239,50.632995248251405;3.15487044695638,50.63278846837585;3.1551519956474863,50.6340291339848")
 walk.photo.attach(io: file, filename: 'heron02.png', content_type: 'image/png')
 
 puts "Create Walk 6/6"
 file = URI.open('https://res.cloudinary.com/dtn8c7o7m/image/upload/v1624540506/drone1_oxzhom.jpg')
-walk = Walk.create!(name: "Tour du complet du lac", description: "Tour complet du lac pour passer une journée à gambader dans la forest et peut-être apercevoir un héron au bord de l'eau.", length: 17, duration: 320,  site: Site.last)
+walk = Walk.create!(name: "Tour du complet du lac", description: "Tour complet du lac pour passer une journée à gambader dans la forest et peut-être apercevoir un héron au bord de l'eau.", length: 41.57966999999999, duration: 52,  site: Site.last, coords:"3.155419751676874,50.635292683622936;3.157350775844634,50.63593253159863;3.1600114005842386,50.63696126423088;3.1603810348600234,50.636612424455535;3.1580829420387317,50.63533017723191;3.155545503598887,50.63490096880932;3.1550694656692713,50.63476985979989;3.154627174138909,50.63248384341517;3.1431814587337783,50.63292476753381;3.1430359246992623,50.63456795027747;3.1422335360711884,50.6356873305441;3.1443637245232594,50.63692547827148;3.146123246652195,50.63628933089174;3.1484777762645137,50.63646184628644;3.151367819172293,50.63648340967245;3.1533823445082874,50.63596047067381;3.1537797752018832,50.63499622656096;3.1551117759649685,50.634912164420285;3.1554166134573904,50.635290448723595")
 walk.photo.attach(io: file, filename: 'heron03.png', content_type: 'image/png')
 
 puts "Walks Created"
